31c4fc86fcbfdccf728fbfe93da65bc225bcfc7a
Lenovo <jannatjui414@gmail.com>
2025-10-23 21:17:19 +0600

    remove extra lines

--- module2/2.8.ts
+++ module2/2.8.ts
@@ -1,57 +1,54 @@
 {
-    // asyncronus typescript
-// basic promise concept
+  // asyncronus typescript
+  // basic promise concept
 
+  // simulate
+  // const createPromise =() : Promise<string> => {
+  //     return new Promise<string>((resolve, reject) => {
+  //         const data : string = 'something'
+  //         if(data){
+  //             resolve(data)
+  //         }
+  //         else{
+  //             reject('fail to load data')
+  //         }
 
-// simulate
-// const createPromise =() : Promise<string> => {
-//     return new Promise<string>((resolve, reject) => {
-//         const data : string = 'something'
-//         if(data){
-//             resolve(data)
-//         }
-//         else{
-//             reject('fail to load data')
-//         }
+  //     })
+  // }
 
-//     })
-// }
+  type Apple = { something: string };
+  type Something = { something: string }; // aikhne string er jaygay something type use kora hoise
 
-type Apple = { something: string };
-type Something = { something: string }; // aikhne string er jaygay something type use kora hoise
-// aikhne string er jaygay something type use kora hoise
+  const createPromise = (): Promise<Apple> => {
+    //aikhne string er jaygay something type use kora hoise
+    return new Promise<Apple>((resolve, reject) => {
+      // aikhne string er jaygay something type use kora hoise
+      const data: Apple = { something: "something" }; // aikhne string er jaygay something type use kora hoise
+      if (data) {
+        resolve(data);
+      } else {
+        reject("fail to load data");
+      }
+    });
+  };
 
-const createPromise =() : Promise<Apple>=>{   //aikhne string er jaygay something type use kora hoise
-    return new Promise<Apple>((resolve, reject)=>{   // aikhne string er jaygay something type use kora hoise
-        const data : Apple = { something: 'something' };   // aikhne string er jaygay something type use kora hoise
-        if(data){
-            resolve(data)
-        }
-        else{
-            reject('fail to load data')
-        }
+  const showData = async (): Promise<Something> => {
+    const data: Something = await createPromise();
+    console.log(data); // output : { something: 'something' }
+    return data;
+  };
 
-    })
-}
+  // calling create function
+  // const showData =async() : Promise <string> =>{
+  // const data : string = await createPromise();
+  // console.log(data)
+  // return data
+  // }
+  //ts-node-dev --respawn --transpile-only .\module2\2.8.ts  terminal a run korle (something) dekhabe
 
-const showData =async() : Promise <Something> =>{
-    const data : Something = await createPromise();
-    console.log(data)     // output : { something: 'something' }
-    return data        
-    }
+  showData();
 
-// calling create function
-// const showData =async() : Promise <string> =>{
-// const data : string = await createPromise();
-// console.log(data) 
-// return data        
-// }
-//ts-node-dev --respawn --transpile-only .\module2\2.8.ts  terminal a run korle (something) dekhabe
-
-
-showData()
-
-type Todo = {
+  type Todo = {
     id: number;
     userId: number;
     title: string;
@@ -66,12 +63,8 @@ type Todo = {
     const data = await response.json();
     console.log(data);
     return data;
-    
   };
 
-  getTodo();   // ts-node-dev --respawn --transpile-only .\module2\2.8.ts  terminal a run korle (something) dekhabe
-    //output : { id: 1, userId: 1, title: 'delectus aut autem', completed: false }
-
-
-
+  getTodo(); // ts-node-dev --respawn --transpile-only .\module2\2.8.ts  terminal a run korle (something) dekhabe
+  //output : { id: 1, userId: 1, title: 'delectus aut autem', completed: false }
 }
