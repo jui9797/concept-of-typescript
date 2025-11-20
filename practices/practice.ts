bc0d876d6ad05d1fceddc159291490d055a54539
Lenovo <jannatjui414@gmail.com>
2025-11-20 15:07:51 +0600

    add two problems

--- practices/practice.ts
+++ practices/practice.ts
@@ -1,7 +1,26 @@
-// Create a function that takes two numbers and returns their sum
+{
+  // Create a function that takes two numbers and returns their sum
 
-function add(a: number, b: number): number {
-  return a + b;
+  function add(a: number, b: number): number {
+    return a + b;
+  }
+
+  console.log(add(5, 7)); // 12
+
+  // Create a type for a user and print the name
+
+  type User = {
+    name: string;
+    email: string;
+    age: number;
+  };
+
+  const user: User = {
+    name: "Jui",
+    email: "jui@example.com",
+    age: 20,
+  };
+
+  console.log(user.name);
+  // Jui
 }
-
-console.log(add(5, 7)); // 12
